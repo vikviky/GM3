ab434c2f102903453269eb9fa352bfc36df767c1
SebMeGizle <XIII@Sebastians-MacBook-Pro.local>
2014-08-20 23:31:30 +0200

    added spinning car, trying to kill

---
+++ gay pride/Assets/Scripts/Car.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+
+	public float carSpeed = 6000; 
+
+	void Update () {
+		transform.Rotate (Vector3.forward * carSpeed * Time.deltaTime, Space.World);
+	
+	}
+}
