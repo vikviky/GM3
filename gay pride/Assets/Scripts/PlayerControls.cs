388b31b8388026508ee53451fb8a00419dcf8029
viktorija <viktorija.gaizutyte@gmail.com>
2014-08-20 20:24:54 +0200

    player control script

---
+++ gay pride/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerControls : MonoBehaviour {
+	
+	//Player Handling
+	public float gravity = 20;
+	public float speed = 8;
+	public float acceleration = 12;
+	
+	private float currentSpeed;
+	private float targetSpeed;
+	private Vector2 amountToMove;
+	
+	private PlayerPhysics playerPhysics;
+	
+	void Start () {
+		playerPhysics = GetComponent <PlayerPhysics> ();
+	}
+	
+	void Update () {
+		targetSpeed = Input.GetAxisRaw ("Horizontal") * speed;
+		currentSpeed = incrementTowards (currentSpeed, targetSpeed, acceleration);
+		
+		amountToMove.x = currentSpeed;
+		amountToMove.y -= gravity * Time.deltaTime;
+		playerPhysics.Move (amountToMove * Time.deltaTime);
+	}
+	
+	//Increasse n towards
+	private float incrementTowards (float n, float target, float a) {
+		if (n == target){
+			return n; 
+		}
+		else { 
+			float dir = Mathf.Sign (target - n);
+			n += a * Time.deltaTime * dir;
+			return (dir == Mathf.Sign(target-n))? n: target;
+			
+		}
+	}
+}
